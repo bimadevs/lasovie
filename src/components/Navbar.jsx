10c57174272798179cdf5fcd23f958fc0a71480d
bimadev0602 <bimadev0602@gmail.com>
2024-11-14 09:08:19 +0000

    ✨update style navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 function Navbar() {
     const navigate = useNavigate()
@@ -41,68 +41,70 @@ function Navbar() {
 
                 {/* Logo Only - Center */}
                 <a onClick={() => navigate('/')} className="flex cursor-pointer items-center mx-auto">
-                    <img 
-                        src="images/logo.png" 
-                        alt="Lasovie Logo" 
+                    <img
+                        src="images/logo.png"
+                        alt="Lasovie Logo"
                         className='w-20 h-10 object-contain' // Adjust size here
                     />
                 </a>
 
-                {/* Mobile Menu with Animation */}
+                {/* Menu with Animation */}
                 <div
-                    className={`fixed inset-0 bg-gray-800 bg-opacity-50 z-50  ${
-                        isMobileMenuOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
-                    } transition-opacity duration-300`}
+                    className={`fixed inset-0 bg-gray-800 bg-opacity-50 z-50  ${isMobileMenuOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
+                        } transition-opacity duration-300`}
                     onClick={toggleMobileMenu} // Click outside to close
                 >
                     <div
-                        className={`w-2/3 bg-white h-full fixed left-0 p-4 space-y-4 shadow-lg transform ${
-                            isMobileMenuOpen ? 'translate-x-0' : '-translate-x-full'
-                        } transition-transform duration-300`}
+                        className={`w-2/3 md:w-1/2 bg-white h-full fixed  left-0 p-4 space-y-4 shadow-lg transform ${isMobileMenuOpen ? 'translate-x-0' : '-translate-x-full'
+                            } transition-transform duration-300`}
                         onClick={(e) => e.stopPropagation()} // Prevent click outside from closing the menu
                     >
-                        <a
-                            onClick={() => navigate('/')}
-                            className="md:text-2xl cursor-pointer block text-gray-700"
-                        >
-                            Home
-                        </a>
-                        <div className="relative">
-                            <button
-                                onClick={toggleDropdown}
-                                className="md:text-2xl block w-full text-left text-gray-700 hover:text-gray-800 focus:outline-none"
+                        <div className="flex flex-col justify-center space-y-5 w-full h-full">
+                            <a
+                                onClick={() => navigate('/')}
+                                className="md:text-2xl cursor-pointer block text-gray-700"
                             >
-                                Kategori
-                            </button>
-                            {isDropdownOpen && (
-                                <div className="bg-white shadow-lg rounded-lg mt-2 py-2">
-                                    <a
-                                        onClick={() => navigate('/atasan')}
-                                        className="md:text-xl block px-4 py-2 text-gray-700 hover:bg-gray-300"
-                                    >
-                                        Atasan
-                                    </a>
-                                    <a
-                                        onClick={() => navigate('/bawahan')}
-                                        className="md:text-xl block px-4 py-2 text-gray-700 hover:bg-gray-300"
-                                    >
-                                        Bawahan
-                                    </a>
-                                    <a
-                                        onClick={() => navigate('/hijab')}
-                                        className="md:text-xl block px-4 py-2 text-gray-700 hover:bg-gray-300"
-                                    >
-                                        Hijab
-                                    </a>
-                                </div>
-                            )}
+                                Home
+                            </a>
+                            <hr />
+                            <div className="relative">
+                                <button
+                                    onClick={toggleDropdown}
+                                    className="md:text-2xl block w-full text-left text-gray-700 hover:text-gray-800 focus:outline-none"
+                                >
+                                    Kategori
+                                </button>
+                                {isDropdownOpen && (
+                                    <div className="bg-white shadow-lg rounded-lg mt-2 py-2">
+                                        <a
+                                            onClick={() => navigate('/atasan')}
+                                            className="md:text-xl block px-4 py-2 text-gray-700 hover:bg-gray-300"
+                                        >
+                                            Atasan
+                                        </a>
+                                        <a
+                                            onClick={() => navigate('/bawahan')}
+                                            className="md:text-xl block px-4 py-2 text-gray-700 hover:bg-gray-300"
+                                        >
+                                            Bawahan
+                                        </a>
+                                        <a
+                                            onClick={() => navigate('/hijab')}
+                                            className="md:text-xl block px-4 py-2 text-gray-700 hover:bg-gray-300"
+                                        >
+                                            Hijab
+                                        </a>
+                                    </div>
+                                )}
+                            </div>
+                            <hr />
+                            <a
+                                onClick={() => navigate('/market')}
+                                className="md:text-2xl block text-gray-700 "
+                            >
+                                Where to Buy?
+                            </a>
                         </div>
-                        <a
-                            onClick={() => navigate('/market')}
-                            className="md:text-2xl block text-gray-700 "
-                        >
-                            Where to Buy
-                        </a>
                     </div>
                 </div>
             </div>
