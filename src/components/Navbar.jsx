9fff411acb057755a48cf16a1d474a5a9e6bf7b8
bimadev0602 <bimadev0602@gmail.com>
2024-11-14 08:41:31 +0000

    🔧fixed hero space

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ function Navbar() {
     };
 
     return (
-        <nav className="fixed top-0 left-0 right-0 z-50 bg-white shadow-md">
+        <nav className="sticky top-0 left-0 right-0 z-50 bg-white shadow-md">
             <div className="container mx-auto px-4 py-4 flex justify-between items-center">
                 {/* Hamburger Menu Button - Left */}
                 <button
